4f810bc36790c6f1d5de9b9995bed11ba85ca0c7
Mazen Chami <mazenchami@gmail.com>
2024-01-04 16:34:59 -0500

    installing packages needed

--- android/app/src/main/java/com/reactnativeessentials/MainActivity.kt
+++ android/app/src/main/java/com/reactnativeessentials/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.reactnativeessentials
+import expo.modules.ReactActivityDelegateWrapper
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -18,5 +20,9 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
