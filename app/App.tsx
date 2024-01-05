03dcb2c5bc9f1ae382a0678a1b4441caf036c101
Mazen Chami <mazenchami@gmail.com>
2024-01-05 11:25:22 -0500

    creating app context

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {AppNavigator} from './navigators/AppNavigator';
+import {GameProvider} from './services/state';
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 
 const App = (): React.JSX.Element => {
-  return <AppNavigator />;
+  return (
+    <GameProvider>
+      <AppNavigator />
+    </GameProvider>
+  );
 };
 
 export default App;
