07ef23ad68cab0589bab8b8ce5873ad5b7f93b76
Yulian Glukhenko <yulian@infinite.red>
2024-01-06 17:47:47 -0800

    Generates tokens from figma.

---
+++ app/theme/tokens/colorTokens.ts
@@ -0,0 +1,41 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 07 Jan 2024 01:47:00 GMT
+ */
+
+export const backgroundButtonPrimaryBase = "#dbff00ff";
+export const backgroundButtonPrimaryPressed = "#490448ff";
+export const backgroundButtonPrimaryDisabled = "#d3c5d3ff";
+export const backgroundButtonSecondaryBase = "#ffffff00";
+export const backgroundButtonSecondaryPressed = "#490448ff";
+export const backgroundButtonSecondaryDisabled = "#ccccccff";
+export const backgroundHeaderList = "#dbff00ff";
+export const backgroundHeaderDetails = "#dbff00ff";
+export const backgroundPillBase = "#490448ff";
+export const backgroundSwitchTrackOn = "#490448ff";
+export const backgroundSwitchTrackOff = "#ae84aeff";
+export const backgroundSwitchThumbOn = "#dbff00ff";
+export const backgroundSwitchThumbOff = "#490448ff";
+export const backgroundCardReflection = "#000000ff";
+export const backgroundSurface100 = "#ffffffff";
+export const backgroundSurface200 = "#e6e6e6ff";
+export const backgroundOverlay = "#abc70599";
+export const borderBase = "#000000ff";
+export const borderRatingActive = "#f10e16ff";
+export const borderButtonPrimaryBase = "#000000ff";
+export const borderButtonPrimaryPressed = "#000000ff";
+export const borderButtonPrimaryDisabled = "#9b649bff";
+export const borderButtonSecondaryBase = "#000000ff";
+export const borderButtonSecondaryPressed = "#000000ff";
+export const borderButtonSecondaryDisabled = "#999999ff";
+export const borderAccent = "#490448ff";
+export const textBase = "#000000ff";
+export const textButtonPrimaryBase = "#000000ff";
+export const textButtonPrimaryPressed = "#dbff00ff";
+export const textButtonPrimaryDisabled = "#9b649bff";
+export const textButtonSecondaryBase = "#000000ff";
+export const textButtonSecondaryPressed = "#dbff00ff";
+export const textButtonSecondaryDisabled = "#808080ff";
+export const textEmptyBase = "#b3b3b3ff";
+export const textAccent = "#490448ff";
+export const textPillBase = "#dbff00ff";
---
+++ app/theme/tokens/sizeRadiusSemantics.ts
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 07 Jan 2024 01:47:00 GMT
+ */
+
+export const lg = 24;
+export const md = 16;
+export const sm = 12;
