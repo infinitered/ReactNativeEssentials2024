4dba8b863fa8af302869f5c348ce7aa68f5220d2
Yulian Glukhenko <yulian@infinite.red>
2024-01-06 18:58:31 -0800

    Adds font.

--- app/App.tsx
+++ app/App.tsx
@@ -1,16 +1,30 @@
+import {useFonts} from 'expo-font';
 import React from 'react';
 import {AppNavigator} from './navigators/AppNavigator';
 import {GlobalStateProvider} from './services/state';
+import {customFontsToLoad} from './theme';
+import {
+  initialWindowMetrics,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 
-const App = (): React.JSX.Element => {
+const App = (): React.JSX.Element | null => {
+  const [areFontsLoaded] = useFonts(customFontsToLoad);
+
+  if (!areFontsLoaded) {
+    return null;
+  }
+
   return (
-    <GlobalStateProvider>
-      <AppNavigator />
-    </GlobalStateProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <GlobalStateProvider>
+        <AppNavigator />
+      </GlobalStateProvider>
+    </SafeAreaProvider>
   );
 };
 
