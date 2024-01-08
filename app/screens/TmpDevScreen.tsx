8990ccbba13d437f2e82040bbf747c251dce3759
Yulian Glukhenko <yulian@infinite.red>
2024-01-07 16:14:43 -0800

    Adds nav and state persistence.

--- app/screens/TmpDevScreen.tsx
+++ app/screens/TmpDevScreen.tsx
@@ -4,6 +4,7 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context'
 import {Button} from '../components/Button'
 import {Text} from '../components/Text'
 import {colors, sizes} from '../theme'
+import {useNavigation} from '@react-navigation/native'
 
 const text = 'Thank you, Mario! But our princess is in another castle!'
 const button = 'Get over here!'
@@ -11,11 +12,13 @@ const button = 'Get over here!'
 export function TmpDevScreen() {
   const {bottom} = useSafeAreaInsets()
 
+  const {navigate} = useNavigation()
+
   return (
     <ScrollView
       style={$scrollView}
       contentContainerStyle={[$contentContainerStyle, {paddingBottom: bottom}]}>
-      <Button text={button} icon="sun" />
+      <Button text={button} icon="sun" onPress={() => navigate('GamesList')} />
       <Button text={button} style={$button} />
       <Text preset="display" text={text} />
       <Text preset="headline1" text={text} />
