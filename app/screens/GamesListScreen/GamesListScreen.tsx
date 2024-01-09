aa2ceb9ab958324508e08c182456f6bfd199fd02
Yulian Glukhenko <yulian@infinite.red>
2024-01-08 22:16:39 -0800

    Adds basic details screen.

--- app/screens/GamesListScreen/GamesListScreen.tsx
+++ app/screens/GamesListScreen/GamesListScreen.tsx
@@ -1,6 +1,55 @@
-import React from 'react'
-import {Text} from '../../components/Text'
+import {useNavigation} from '@react-navigation/native'
+import React, {useCallback, useEffect} from 'react'
+import {FlatList, ViewStyle} from 'react-native'
+import {useSafeAreaInsets} from 'react-native-safe-area-context'
+import {Card} from '../../components/Card'
+import {api} from '../../services/api'
+import {useGlobalState} from '../../services/state'
+import {colors, sizes} from '../../theme'
 
 export const GamesListScreen = () => {
-  return <Text text=" Games List Screen" />
+  const {bottom: paddingBottom} = useSafeAreaInsets()
+
+  const navigation = useNavigation()
+
+  const {setGames, games} = useGlobalState()
+
+  const getGames = useCallback(async () => {
+    const response = await api.getGames()
+
+    if (response.ok) {
+      setGames(response.data)
+    }
+  }, [setGames])
+
+  useEffect(() => {
+    getGames()
+  }, [getGames])
+
+  return (
+    <FlatList
+      data={games}
+      style={$list}
+      keyExtractor={item => String(item.id)}
+      contentContainerStyle={[{paddingBottom}, $contentContainer]}
+      renderItem={({item}) => (
+        <Card
+          onPress={() => navigation.navigate('GameDetails', {gameId: item.id})}
+          name={item.name}
+          rating={item.totalRatingStarsRounded}
+          releaseDate={item.releaseDates?.[0]?.human}
+          imageUrl={item.cover.imageUrl}
+        />
+      )}
+    />
+  )
+}
+
+const $list: ViewStyle = {
+  backgroundColor: colors.tokens.backgroundSurface100,
+}
+
+const $contentContainer: ViewStyle = {
+  rowGap: sizes.spacing.lg,
+  paddingHorizontal: sizes.spacing.md,
 }
